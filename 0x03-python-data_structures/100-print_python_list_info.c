970c1f41710aa5a5685f234683d19b689f7feaee
doubix <adib.grouz@gmail.com>
2019-12-17 20:50:29 +0100

    fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <Python.h>
 /**
- * print_python_list_info - print basic info about Python lists
- * @p: python object pointer
- * Return: Void.
- */
-
+* print_python_list_info - prints python list info
+* @p: PyObject
+* Return: no return
+*/
 void print_python_list_info(PyObject *p)
 {
 PyListObject *list;
-Py_ssize_t size, i;
-PyObject *object;
-struct _typeobject *type;
+Py_ssize_t l_size, i;
+PyObject *obj;
+struct _typeobject *el_type;
 
 if (strcmp(p->ob_type->tp_name, "list") == 0)
 {
 list = (PyListObject *)p;
-size = list->ob_base.ob_size;
-printf("[*] Size of the Python List = %ld\n", size);
+l_size = list->ob_base.ob_size;
+printf("[*] Size of the Python List = %ld\n", l_size);
 printf("[*] Allocated = %ld\n", list->allocated);
-for (i = 0; i < size; i++)
+for (i = 0; i < l_size; i++)
 {
-object = list->ob_item[i];
-type = object->ob_type;
-printf("Element %ld: %s\n", i, type->tp_name);
+obj = list->ob_item[i];
+el_type = obj->ob_type;
+printf("Element %ld: %s\n", i, el_type->tp_name);
 }
 }
 }

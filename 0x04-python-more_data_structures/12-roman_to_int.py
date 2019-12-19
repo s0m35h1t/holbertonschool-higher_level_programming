0878c85b5970822b1290caff889c3e670d8f2d31
doubix <adib.grouz@gmail.com>
2019-12-19 16:36:11 +0100

    fix return if not str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_num = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    sum = 0
-    for i in roman_string:
-        sum += roman_num[i]
-    return sum
+    if type(roman_string) is not str:
+        return None
+    if roman_string:
+        roman_num = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        sum = 0
+        for i in roman_string:
+            sum += roman_num[i]
+        return sum
+    return (0)

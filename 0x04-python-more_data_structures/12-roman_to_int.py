a4439dc122213355b5043ce22666b0dabd374626
doubix <adib.grouz@gmail.com>
2019-12-19 16:42:36 +0100

    fix test string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
+    if roman_string and type(roman_string) is str:
         roman_num = {
             'I': 1,
             'V': 5,
@@ -12,6 +12,8 @@ def roman_to_int(roman_string):
         }
         sum = 0
         for i in roman_string:
+            if i not in roman_num.keys():
+                return 0
             sum += roman_num[i]
         return sum
     return (0)

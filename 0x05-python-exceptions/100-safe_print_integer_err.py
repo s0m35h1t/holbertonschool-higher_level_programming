e28967032a3f4b972dad07694aa74c6a680d62fd
doubix <adib.grouz@gmail.com>
2020-01-08 18:03:29 +0100

    [FIX] blank line

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
+        import sys
         print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:

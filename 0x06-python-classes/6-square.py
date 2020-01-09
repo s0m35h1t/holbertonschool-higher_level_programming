762981498b8d4dec57fbd4ffd65ec345e71a096f
doubix <adib.grouz@gmail.com>
2020-01-09 23:53:29 +0100

    [update] class function

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -94,6 +94,6 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for j in range(self.__size):
-                print("".join([" " for k in range(self.__position[0])]), end="")
+                print(
+                    "".join([" " for k in range(self.__position[0])]), end="")
                 print("".join(["#" for l in range(self.__size)]))
-

a64518a4cdafb33a61b15c9b13f512441d7d1a09
doubix <adib.grouz@gmail.com>
2020-01-17 15:10:13 +0100

    [fix] setters

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,6 +57,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
@@ -73,6 +74,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """calculates the rectangle's area

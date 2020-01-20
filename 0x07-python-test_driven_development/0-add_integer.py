0cfa196314fa7e0ab704f4f0b9c78283daec625b
doubix <adib.grouz@gmail.com>
2020-01-20 16:56:07 +0100

    if elif statement

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,9 +15,9 @@ def add_integer(a, b=98):
     Returns:
         int
     """
-
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    else:
+        return int(a) + int(b)

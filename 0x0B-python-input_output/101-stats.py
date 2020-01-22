7f2618b9c4c1937e229c348a1981e41a1f9ff8d7
doubix <adib.grouz@gmail.com>
2020-01-22 22:21:59 +0100

    [fix] print when exception

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,7 +3,7 @@
 reads stdin line by line and computes metrics
 """
 import sys
-
+import time
 file_size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
@@ -37,4 +37,5 @@ if __name__ == "__main__":
             status_print(status_tally, file_size)
 
     except KeyboardInterrupt as e:
+        status_print(status_tally, file_size)
         print(e)

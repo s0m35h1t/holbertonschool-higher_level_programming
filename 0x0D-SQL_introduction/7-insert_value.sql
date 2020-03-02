7fff09c79209b07949d9a999a82beb30db6efc7a
doubix <adib.grouz@gmail.com>
2020-03-02 17:48:59 +0100

    [fix] end colon

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -4,8 +4,5 @@
 --     name = Holberton School
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO `first_table` (`
-id`,
-`name
-`) VALUES
-(89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
+

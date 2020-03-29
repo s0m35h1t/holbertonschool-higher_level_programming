78ca3f75acffd3e7eb8cf440938e5bf986e5df26
doubix <adib.grouz@gmail.com>
2020-03-29 22:30:35 +0100

    [update] models relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,4 @@ class State(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
-                          cascade="save-update, delete")
+                          cascade="all, delete")
